642d5b2c0d73e9cd6be855ca12b5c2231e7cc834
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2025-05-18 21:18:54 +0200

    Fixed typescript (not) transpiling stuff

--- src/commands/utils/limit.ts
+++ src/commands/utils/limit.ts
@@ -7,7 +7,7 @@ import {
   SlashCommandBuilder,
   TextDisplayBuilder,
 } from "discord.js";
-import { increaseLimit, ratelimit } from "~/utils/ratelimit.js";
+import { increaseLimit, ratelimit } from "../../utils/ratelimit.js";
 
 export default {
   data: new SlashCommandBuilder()
